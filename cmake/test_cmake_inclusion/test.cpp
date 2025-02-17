afcbc17ef2ca927612dba732539cdc222be806bf
Davide Stocco <davide.stocco@unitn.it>
2025-02-17 11:42:35 +0100

    catch_discover_tests included

--- cmake/test_cmake_inclusion/test.cpp
+++ cmake/test_cmake_inclusion/test.cpp
@@ -1,8 +1,10 @@
 #include "Optimist.hh"
+#include "Optimist/TestSet.hh"
 #include <iostream>
 
 int main()
 {
   Optimist::Info(std::cout);
+  Optimist::TestSet::TestSetInfo(std::cout);
   return 0;
 }
