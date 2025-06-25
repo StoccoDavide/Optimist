93f4a77219e0bb85c2b3af29d653b137fae53c6d
Davide Stocco <davide.stocco@unitn.it>
2025-06-25 16:47:11 +0200

    CMake prefix path append removed

--- cmake/OptimistEigen3.cmake
+++ cmake/OptimistEigen3.cmake
@@ -1,7 +1,7 @@
 set(EIGEN_REQUIRED_VERSION 3.4.0)
 cmake_policy(SET CMP0135 NEW)
 
-list(APPEND CMAKE_PREFIX_PATH "${OPTIMIST_THIRD_PARTY_DIR}")
+# list(APPEND CMAKE_PREFIX_PATH "${OPTIMIST_THIRD_PARTY_DIR}")
 find_package(
   Eigen3
   ${EIGEN_REQUIRED_VERSION}
@@ -17,7 +17,8 @@ if(NOT TARGET Eigen3::Eigen)
   set(FETCHCONTENT_BASE_DIR "${OPTIMIST_THIRD_PARTY_DIR}")
   fetchcontent_declare(
     Eigen3
-    URL "https://gitlab.com/libeigen/eigen/-/archive/${EIGEN_REQUIRED_VERSION}/eigen-${EIGEN_REQUIRED_VERSION}.tar.gz")
+    URL "https://gitlab.com/libeigen/eigen/-/archive/${EIGEN_REQUIRED_VERSION}/eigen-${EIGEN_REQUIRED_VERSION}.tar.gz"
+  )
 
   fetchcontent_makeavailable(Eigen3)
 else()
